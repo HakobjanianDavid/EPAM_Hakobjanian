1134f313ba3bfc1e9ea21c4bb94feeb23c9d0048
David <hakobjaniandavid@gmail.com>
2020-03-12 01:30:56 +0400

    fix: change password, login, number and card owner checking

--- Task2(JS-Advanced)/Main/src/js/checkFunctions.js
+++ Task2(JS-Advanced)/Main/src/js/checkFunctions.js
@@ -1,9 +1,12 @@
-function checkEmail(email) {
+function checkLogin(pLogin) {
+    if(!pLogin) {
+        return false;
+    }
     const atComercialRegExp = /@/g;
-    const atComercial = email.split(atComercialRegExp);
+    const atComercial = pLogin.split(atComercialRegExp);
     let firstAndSecondLevelDomen = atComercial.length === 2 ? atComercial[1] : false;
 
-    if(firstAndSecondLevelDomen) {
+    if (firstAndSecondLevelDomen) {
         const compositeRegExp = /@[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.(com|edu|gov|info|net|org|ru)/g;
         const result = compositeRegExp.test(`@${firstAndSecondLevelDomen}`) ? true : false;
         return result;
@@ -12,19 +15,22 @@ function checkEmail(email) {
     }
 }
 
-function checkPassword(password) {
+function checkPassword(pPassword) {
+    if(!pPassword) {
+        return false;
+    }
     const numberAndLetterRegExp = /[^a-z0-9]/gi;
     let result = false;
 
-    if ( password.length > 8 ) {
-        const specSimbol = password.match(numberAndLetterRegExp) || [];
+    if ( pPassword.length > 8) {
+        const specSimbol = pPassword.match(numberAndLetterRegExp) || [];
         result = specSimbol.length === 0 ? true : false;
     }
     return result;
 }
 
 function checkCardNumber(number) {
-    if (number.length <= 12) {
+    if (number.length >= 8 && number.length <= 12) {
         number = parseInt(number);
         const rangeRegExp = /[^0-9]/g;
 
@@ -48,34 +54,4 @@ function checkCardCvvCode(code) {
     return codeRegExp.test(code);
 }
 
-function passwordAndLoginChecking (input, checkFunc, trueReturnedFunction, falseReturnedFunction, alertNote) {
-    if( input !== '' && input !== null) {
-        if(checkFunc(input)) {
-            return trueReturnedFunction(); 
-        } else {
-            alert(alertNote);
-            return falseReturnedFunction();
-        }
-    } else {
-        alert(alertNote);
-        return falseReturnedFunction();
-    }
-}
-
-function numberAndOwnerCheck(input, checkFunc, nextFunction, mainMenuReturn, wrongAnswerFunction, alertNote) {
-    if( input !== '' && input !== null) {
-        if (checkFunc(input) && input !== '2') {
-            return nextFunction();
-        }
-        if (input === '2') {
-            return mainMenuReturn();
-        } else {
-            return wrongAnswerFunction();
-        }
-    } else {
-        alert(alertNote);
-        return wrongAnswerFunction();
-    }
-}
-
-export {checkEmail, checkPassword, checkCardNumber, checkCardOwner, checkCardCvvCode, passwordAndLoginChecking, numberAndOwnerCheck};
+export { checkLogin, checkPassword, checkCardNumber, checkCardOwner, checkCardCvvCode };
